03b6a6b6ea5b909d976f16f41e69cf3f545fb947
rshevchenko <rshevchenko@webmd.net>
2020-01-02 12:58:06 +0300

    loginUI added

--- app/src/app/components/login/login.component.ts
+++ app/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+import {AuthorizationServiceService} from '../../services/authorization-service.service';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginUserData = {};
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  passFormControl = new FormControl('', [
+    Validators.required,
+  ]);
+
+  constructor(private _auth: AuthorizationServiceService) { }
 
   ngOnInit() {
+  }
+
+  loginUser() {
+    this._auth.loginUser(this.loginUserData)
+      .subscribe( res => console.log(res), err => console.log(err));
   }
 
 }
--- app/src/app/services/authorization-service.service.ts
+++ app/src/app/services/authorization-service.service.ts
@@ -6,11 +6,21 @@ import {HttpClient} from '@angular/common/http';
 })
 export class AuthorizationServiceService {
 
-  private _registrationUrl = 'http://localhost:3333/api/register';
+  serverUrl = 'http://localhost:3333/api';
+
+  private _registrationUrl = `${this.serverUrl}/register`;
+  private _loginUrl = `${this.serverUrl}/login`;
+
   constructor(private http: HttpClient) {
   }
 
   registerUser(user) {
     return this.http.post<any>(this._registrationUrl, user);
   }
+
+  loginUser(user) {
+    return this.http.post<any>(this._loginUrl, user);
+  }
+
+
 }
